60710f30f8858e5625f9ad93737210544eae5cdf
504 <504@DESKTOP-CNRDTLS>
2022-05-26 15:52:47 +0900

    05/26 15:52 최으뜸 DAO 생성(AccountDao 추가)

---
+++ coconutBank/src/main/java/dao/AccountDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+public class AccountDao extends Dao {
+
+	public AccountDao() {
+		super();
+	}
+	// 호출용
+	public static AccountDao accountDao = new AccountDao();
+	public static AccountDao getaccAccountDao() {return accountDao;}
+		
+} // class end
