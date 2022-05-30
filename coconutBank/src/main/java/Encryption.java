85027182b99ecb1d7d69592043526ec8f26b561a
504 <504@DESKTOP-CNRDTLS>
2022-05-30 18:01:02 +0900

    05/30 18:00 최으뜸 암호화키test

--- coconutBank/src/main/java/Encryption.java
+++ coconutBank/src/main/java/Encryption.java
@@ -1,11 +1,19 @@
 import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Encryption {
 
+/////////////////////////////////////////////////////////////// 계좌번호 생성 //////////////////////////////////////////////////////////////////////	
+	
 	// 계좌번호 생성 메소드
 	public String getaccountno() {
 		Random rd = new Random();
@@ -13,7 +21,9 @@ public class Encryption {
 				rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10));
 		return accountno;
 	} // 계좌번호 생성 end
-	
+
+/////////////////////////////////////////////////////////////// SHA256 / SALT 사용 //////////////////////////////////////////////////////////////////////	
+
 	// salt 생성 메소드
 	public String getsalt() {
 		String salt = "";
@@ -25,10 +35,22 @@ public class Encryption {
 		}catch(Exception e) {System.out.println("salt생성시 오류 : "+e);}
 		return salt;
 	} // salt생성 end
+
+	// 테스트용
+	String acno = "112-120-120120";
+	String pw = "0536";
+	
+	// 계좌+비번+추가 
+	public String dbpassword(String acno, String pw) {
+		String password = acno.replace("-", "")+pw;
+		int password2 = Integer.parseInt(password)*2022228;
+		System.out.println(password2);
+		return null;
+	}
 	
 	// SHA256 해시함수 사용 메소드
-	public String sha256(String salt, String password) {
-		String saltpw = salt+password;
+	public String sha256(String acno, String pw) {
+		String password = acno+pw;
 		String hex = null;
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -37,6 +59,46 @@ public class Encryption {
 		}catch(Exception e) {System.out.println("256암호화시 오류 : "+e);}
 		return hex;
 	} // 암호화 비밀번호 end
+	
+
+/////////////////////////////////////////////////////////////// RSA 사용 //////////////////////////////////////////////////////////////////////	
+	
+	// 
+	static final int key_size = 2048;
+	
+	// 키페어생성 메소드
+	static HashMap<String, String> createkeypair(){
+		HashMap<String , String> stringkeypair = new HashMap<String, String>();
+		try {
+			SecureRandom srd = new SecureRandom();
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			kpg.initialize(key_size, srd);
+			KeyPair keyPair = kpg.genKeyPair();
+			
+			PublicKey publicKey = keyPair.getPublic();
+			PrivateKey privateKey = keyPair.getPrivate();
+			
+			String stringPublickey = Base64.getEncoder().encodeToString(publicKey.getEncoded() );
+			String stringPrivatekey = Base64.getEncoder().encodeToString(privateKey.getEncoded() );
+			
+			stringkeypair.put("publicKey", stringPublickey);
+			stringkeypair.put("privateKey", stringPrivatekey);
+			
+		}catch(Exception e) {System.out.println("키페어생성오류 : "+e);}
+		return stringkeypair;
+	} // 키페어생성 end
+	
+	// 암호화 메소드
+	static String encode(String acps, String stringPublickey) {
+		String encryptedData = null;
+		try {
+			// 평문으로 전달받은 공개키를 공개키객체로 만드는 과정
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			byte[] bytePublickey = Base64.getDecoder().decode(stringPublickey.getBytes() );
+			
+		}catch(Exception e) {System.out.println("암호화오류 : "+e);}
+	} // 암호화 end
+	
 	
 	
 	public static void main(String[] args) {
@@ -62,22 +124,23 @@ public class Encryption {
 //		
 //		}catch(Exception e) {System.out.println("salt생성시 오류 : "+e);}
 		
-		Random rd = new Random();
-		String accountno = ("112-"+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+"-"+
-				rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10)+rd.nextInt(10));
-		String pw = "2345";
-		System.out.println("accountno :" +accountno);
-		System.out.println("사용자 pw : "+pw);
+		HashMap<String, String> rsakeypair = createkeypair();
+		String publickey = rsakeypair.get("publicKey");
+		String privatekey = rsakeypair.get("privateKey");
 		
-			String saltpw = accountno+pw;
-			System.out.println("계좌번호+비밀번호 : "+saltpw);
-			String hex = null;
-			try {
-				MessageDigest md = MessageDigest.getInstance("SHA-256");
-				md.update(saltpw.getBytes());
-				hex = String.format("%64x", new BigInteger(1, md.digest()));
-				System.out.println("256해시함수 : "+hex);
-			}catch(Exception e) {System.out.println("256암호화시 오류 : "+e);}
+		System.out.println("만들어진 공개키 : "+publickey);
+		System.out.println("만들어진 개인키 : "+privatekey);
+
+		
+		String acno = "112-120-120120";
+		String pw = "0536";
+		String password = acno.replace("-", "")+pw;
+		System.out.println(password);
+		int password2 = Integer.parseInt(password);
+		System.out.println(password2);
+		
+		
+		
 			
 		
 		
